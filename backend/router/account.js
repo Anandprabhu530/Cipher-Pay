fa8320bb544c2cb87d251668cc8580684cedb493
Anand Prabhu <anandprabhu530@gmail.com>
2024-07-10 20:43:43 +0530

    Added balance check

--- backend/router/account.js
+++ backend/router/account.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const { User, Account } = require("../db");
 
-const Accoutrouter = express.Router();
+const router = express.Router();
 
-module.exports = Accoutrouter;
+router.get("/balance", async (req, res) => {
+  const username = req.body.username;
+
+  const user_id = await User.findOne({ username: username });
+  if (user_id) {
+    const balance = await Account.findOne({ userId: user_id.id });
+    return res.status(200).json({ balance: balance.balance });
+  }
+  return res
+    .status(411)
+    .json({ message: "Invalid user! Please login or register" });
+});
+
+module.exports = router;
